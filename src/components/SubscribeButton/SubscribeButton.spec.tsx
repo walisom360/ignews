57abe8f417d6c6f35e1d182363b1b853d7bf0e35
walison matheus <walisom360@hotmail.com>
2021-08-14 20:17:34 -0300

    adding subscribeButton test

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,64 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+// import { mocked } from 'ts-jest/utils';
+import { signIn, useSession } from 'next-auth/client';
+import { mocked } from 'ts-jest/utils';
+import { useRouter } from 'next/router';
+import { SubscribeButton } from '.';
+
+jest.mock('next-auth/client');
+
+jest.mock('next/router');
+
+describe('SubscribeButton component', () => {
+  it('it renders correctly', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SubscribeButton />);
+
+    expect(screen.getByText('Subscribe now')).toBeInTheDocument();
+  });
+
+  it('redirects user to sign in when not authenticated', () => {
+    const signInMocked = mocked(signIn);
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it('redirects to post when user already has subscription', () => {
+    const useRouterMocked = mocked(useRouter);
+    const useSessionMocked = mocked(useSession);
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: { name: 'John Doe', email: 'john.doe@example.com' },
+        expires: 'fake-expires',
+        activeSubscription: 'fake-active-subscription',
+      },
+      false,
+    ]);
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton);
+
+    expect(pushMock).toHaveBeenCalledWith('/posts');
+  });
+});
